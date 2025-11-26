ce3b3f48d1492cecd4c1f3111bc38b4976b9dd84
Dayesoro <dayesoro@gmail.com>
2025-11-26 23:02:18 +0000

    feat: add request logging middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const logger = require('./middleware/logger');
 
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 
 app.get('/health', (req, res) => {
     res.json({ message: 'Library API is running' });
